a7afca0ce881299ded4142c8601b79e416ddc389
Rajat Sahu <sahurjt@gmail.com>
2017-04-23 01:06:28 +0530

    remove unused code

--- BusTrack/helpers/SessionHelper.py
+++ BusTrack/helpers/SessionHelper.py
@@ -5,6 +5,10 @@ ADMIN_LOGIN = 'is_admin_login'
 ADMIN_LOGIN_ID = 'admin_id'
 TOKEN='token'
 
+'''
+Unused method replaced by jinja2 template function
+'''
+'''
 def nav_list():
     # items to show in navigation list
     nav_logged_in = [('/admin/', 'Home'), ('/admin/driver', 'Add Update Driver'),
@@ -16,22 +20,22 @@ def nav_list():
         return nav_logged_in
     else:
         return nav_logged_out
-
-
+'''
+    # checks weather admin is logged in
 def is_login_admin():
-    # check login session of admin
     if ADMIN_LOGIN in session:
         if session[ADMIN_LOGIN] == True:
             return True
     else:
         return False
 
+    ''''using bcryot to store password hash, key is given in projects config.py'''
 
+    # generate password hash for @password
 def get_password_hash(password):
     return bcrypt.generate_password_hash(password)
 
-
+# check validity of @password with the given @pass_hash
 def is_password_correct(pass_hash, password):
     # return true if correct
     return bcrypt.check_password_hash(pass_hash, password)
-
