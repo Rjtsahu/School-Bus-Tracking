d4d8b08acdb422c0a6565663d5e412d13a7a2f98
Rajat Sahu <sahurjt@gmail.com>
2017-04-23 00:51:48 +0530

    added comments

--- run.py
+++ run.py
@@ -1,7 +1,15 @@
+'''
+Import flask 'app' module from BusTrack  __init__.py
+'''
+
 from BusTrack import app
 
 app.config.from_object('config')
 app.config.from_pyfile('config.py')
-#app.config['SQLALCHEMY_DATABASE_URI'] ='sqlite:///data.db'
+
+'''
+This is start up file for project
+'''
+
 if __name__=='__main__':
     app.run(host='0.0.0.0')
