78de65de91f348b932d56965e1fbeb3e07dc99c3
Rajat Sahu <sahurjt@gmail.com>
2018-12-03 23:20:07 +0530

    dev wip for auth decorator for various roles in api,also using session for global access of user object

--- BusTrack/controllers/HelloWorldController.py
+++ BusTrack/controllers/HelloWorldController.py
@@ -1,4 +1,5 @@
 from flask_restful import Resource
+from flask import session
 from BusTrack.services.UserService import UserService
 from BusTrack.repository.schema import users_schema
 
@@ -9,4 +10,8 @@ class HelloWorldController(Resource):
         # data = {'hello': 'world'}
         u = UserService()
         c = u.get_all_user()
+        if 'user' not in session:
+            session['user'] = {}
+            session['user'] = {'a':'a'}# users_schema.jsonify(c)
+        print('session',session['user'])
         return users_schema.jsonify(c)
